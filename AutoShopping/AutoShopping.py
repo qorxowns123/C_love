11c0b4a68a2149200ace02f2b733922f5108f422
tjback <qorxowns123@naver.com>
2017-12-25 20:17:59 +0900

    쿠팡 데이터 가져오기 완료

--- AutoShopping/AutoShopping.py
+++ AutoShopping/AutoShopping.py
@@ -7,6 +7,31 @@ def AutoCopang(driver, keyword):
     driver.get('https://www.coupang.com/')
     # 검색어 입력
     driver.find_element_by_name('q').send_keys(keyword)
+    # 검색 버튼 클릭
+    driver.find_element_by_xpath('//*[@id="headerSearchBtn"]').click()
+    # 웹페이지 소스 추출
+    getHtml = driver.page_source
+    # HTML 소스 읽어오기
+    getParser = BeautifulSoup(getHtml, 'html.parser')
+
+    # 태그를 통한 이름 가져오기aaa
+    CoPangItemNameList = []
+    getTagInfo = getParser.find_all("div", {"class" : "name"})
+    for loopidx in range(0, getTagInfo.__len__()):
+        CoPangItemNameList.append(getTagInfo[loopidx].text)
+
+    # 태그를 통한 가격 가져오기
+    CoPangItemPriceList = []
+    getTagInfo = getParser.find_all("strong", {"class": "price-value"})
+    for loopidx in range(0, getTagInfo.__len__()):
+        CoPangItemPriceList.append(getTagInfo[loopidx].text)
+        
+    # 태그를 통한 이미지 가져오기
+    CoPangItemImgList = []
+    getTagInfo = getParser.find_all("dt", {"class": "image"})
+    for loopidx in range(0, getTagInfo.__len__()):
+        CoPangItemImgList.append(getTagInfo[loopidx].contents[1].attrs['src'])
+
     driver.close()
 
 if __name__ == "__main__":
