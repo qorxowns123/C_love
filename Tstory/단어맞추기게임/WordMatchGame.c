cb048abd63ec3ea9371087e6347ef4a4c7e46d1e
tjback <qorxowns123@naver.com>
2019-10-18 15:23:20 +0900

    단어맞추기게임 거의 완성

--- Tstory/단어맞추기게임/WordMatchGame.c
+++ Tstory/단어맞추기게임/WordMatchGame.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define WORD_CNT	10
@@ -9,15 +10,41 @@ char word_list[WORD_CNT][15] = {"monkey", "elephant", "dog", "sheep", "pig", "li
 void init_game(void);
 char* select_word(void);
 char insert_word(void);
+unsigned char match_string(char* string, char* _string, char word);
 
 void main(void)
 {
-	char *word;
+	char *string;
+	char *_string;
+	char word;
+	unsigned char match_check = 0;
+	unsigned int string_len = 0;
+	unsigned int loopidx = 0;
+
 	init_game();
-	word = select_word();
-	insert_word();
+	string = select_word();
+	string_len = strlen(string);
+	_string = (char*)malloc(sizeof(char) * (string_len + 1));
+
+	for(loopidx = 0; loopidx < string_len; loopidx++)
+	{
+		_string[loopidx] = '_';
+	}
+	_string[string_len] = '\0';
+
+
+	while(match_check == 0)
+	{
+		word = insert_word();
+		match_check = match_string(string, _string, word);
+	}
+
+	printf("### 축하합니다!!\n");
 }
 
+/* ########################## */
+/* ### Sub Function List ### */
+/* ########################## */
 void init_game(void)
 {
 	printf("============================================\n");
@@ -36,7 +63,7 @@ char* select_word(void)
 
 	for(loopidx = 0; loopidx < 9; loopidx++)
 	{
-		select_index = (rand() % 14);
+		select_index = (rand() % 9);
 	}
 
 	return word_list[select_index];
@@ -47,7 +74,46 @@ char insert_word(void)
 	char word;
 
 	printf("<Info> :: 영어 단어를 입력하세요(a~z) : ");
-	scanf("%c", &word);
+	scanf(" %c", &word);
 
 	return word;
+}
+
+unsigned char match_string(char* string, char* _string, char word)
+{
+	unsigned int string_len = 0;
+	unsigned int loopidx = 0;
+	unsigned char return_check = 0;
+
+	static unsigned int check_cnt = 0;
+
+	string_len = strlen(string);
+
+
+	printf("### Result : ");
+	for(loopidx = 0; loopidx < string_len; loopidx++)
+	{
+		if(word == string[loopidx])
+		{
+			_string[loopidx] = word;
+			check_cnt++;
+		}
+		else{/**/}
+	}
+
+	printf("%s\n\n", _string);
+	//printf("\n");
+
+	if(check_cnt == string_len)
+	{
+		return_check = 1;
+	}
+	else
+	{
+		return_check = 0;
+	}
+
+	return return_check;
+
+
 }
